0d05076bb70f339578670973a14bb823f4017327
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-08 13:43:58 -0500

    added confirm for numbers, letters, symbols

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,18 @@ function generateNewPassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var uppers = confirm("Would you like to use uppercase letters?");
+  var lowers = confirm("Would you like to use lowercase letters?");
+  var numbers = confirm("Would you like to use numbers?");
+  var symbols = confirm("Would you like to use special characters?");
+  
+  while (!uppers && !lowers && !numbers && !symbols) {
+    alert("You must select at least one character type!");
+    uppers = confirm("Would you like to use uppercase letters?");
+    lowers = confirm("Would you like to use lowercase letters?");
+    numbers = confirm("Would you like to use numbers?");
+    symbols = confirm("Would you like to use special characters?");
+  }
   passwordText.value = password;
 
 }
