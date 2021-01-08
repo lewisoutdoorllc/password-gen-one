3351bf7b99c44d320dde64e426c83682e4bb1d56
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-08 13:48:24 -0500

    added password lenght minimum and maximum

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,8 @@
-// Assignment code here
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-var generateButton = document.getElementById('generateNewPassword');
-generateButton.addEventListener('click', function() {
-  generateNewPassword();
-});
 
 function generateNewPassword() {
   var length = Number(prompt("How many characters would you like your password to be?"));
-// Write password to the #password input
-function writePassword() {
+
+  while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var uppers = confirm("Would you like to use uppercase letters?");
