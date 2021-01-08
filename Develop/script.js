0bf7ad39845af914bfa3115f2304600be6872696
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-08 15:58:51 -0500

    added comments to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -8,13 +8,13 @@ function generateNewPassword() {
   var length = Number(prompt("How many characters would you like your password to be?"));
 while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
 
-// confirm prompts 
+// confirm prompts for user to choose what inputs they would like included in there password //
 var uppers = confirm("Would you like to use uppercase letters?");
 var lowers = confirm("Would you like to use lowercase letters?");
 var numbers = confirm("Would you like to use numbers?");
 var symbols = confirm("Would you like to use special characters?");
 
-// 
+// if user does not choose any avalible inputs it will return for them to select at least one //
 while (!uppers && !lowers && !numbers && !symbols) {
   alert("You must select at least one character type!");
   uppers = confirm("Would you like to use uppercase letters?");
@@ -25,13 +25,13 @@ while (!uppers && !lowers && !numbers && !symbols) {
 
 
   var password = "";
-
+// this uses the random generator js file that is attacted thru html and is part of this repo so as to have no external link issues or code breaks // 
   var allowed = {};
   if (uppers) password += rando(allowed.uppers = "QWERTYUIOPASDFGHJKLZXCVBNM");
   if (lowers) password += rando(allowed.lowers = "qwertyuiopasdfghjklzxcvbnm");
   if (numbers) password += rando(allowed.numbers = "1234567890");
   if (symbols) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,.");
-
+// this will put out a ramdom password dependent on user choices //
   for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
 
   document.getElementById("password").value = randoSequence(password).join("");
