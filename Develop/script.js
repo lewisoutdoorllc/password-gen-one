de4f1067f83c3e9beb0b7183a25442cdd65efff8
Cody <74194707+lewisoutdoorllc@users.noreply.github.com>
2021-01-08 13:37:31 -0500

    working on button function

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,13 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var generateButton = document.getElementById('generateNewPassword');
+generateButton.addEventListener('click', function() {
+  generateNewPassword();
+});
 
+function generateNewPassword() {
+  var length = Number(prompt("How many characters would you like your password to be?"));
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
